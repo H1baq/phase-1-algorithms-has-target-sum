146c3b23d2f3af390d218019a5b7d290fb07f9c9
hibby <hibaqku7@gmail.com>
2025-03-23 11:52:40 +0300

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 function hasTargetSum(array, target) {
+  const seenNumbers = {};
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+
+  return false;
+
   // Write your algorithm here
 }
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
+}
 
+function findSock(object) {
+  if (object.sock) return "sock";
+}
+function findSock(object) {
+  if (object.sock) return "sock";
+}
 /* 
   Write the Big O time complexity of your function here
 */
